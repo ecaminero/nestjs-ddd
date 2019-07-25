544548a82621020b5d8e902ef67c75d0168f0a71
ecaminero <caminero.ediwn@gmail.com>
2019-07-25 12:17:32 -0400

    Adding health check

--- src/infrastructure/health/terminus-options.check.ts
+++ src/infrastructure/health/terminus-options.check.ts
@@ -5,7 +5,9 @@ import {
   TerminusModuleOptions,
   MemoryHealthIndicator,
   MongooseHealthIndicator,
+  MicroserviceHealthIndicator,
 } from '@nestjs/terminus';
+import { Transport } from '@nestjs/microservices';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
@@ -14,6 +16,7 @@ export class TerminusOptionsService implements TerminusOptionsFactory {
     private readonly dns: DNSHealthIndicator,
     private readonly memory: MemoryHealthIndicator,
     private readonly mongoose: MongooseHealthIndicator,
+    private readonly microservice: MicroserviceHealthIndicator,
   ) {}
 
   createTerminusOptions(): TerminusModuleOptions {
@@ -24,6 +27,11 @@ export class TerminusOptionsService implements TerminusOptionsFactory {
         async () => this.memory.checkHeap('memory_heap', 200 * 1024 * 1024),
         async () => this.memory.checkRSS('memory_rss', 3000 * 1024 * 1024),
         async () => this.mongoose.pingCheck('mongo'),
+        async () => this.microservice.pingCheck('nats', {
+          transport: Transport.NATS,
+          options: { url: 'nats://127.0.0.2:4222' },
+        }),
+
       ],
     };
     return {
